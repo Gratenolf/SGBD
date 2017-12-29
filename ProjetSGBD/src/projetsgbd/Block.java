928408f251cb417043a40cfe8a5d11a45f253368
Nicolas Godinho <godinhonicolas@hotmail.fr>
2017-12-29 18:20:06 +0100

    Add files via upload

--- ProjetSGBD/src/projetsgbd/Block.java
+++ ProjetSGBD/src/projetsgbd/Block.java
@@ -1,24 +1,58 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package projetsgbd;
+import java.util.ArrayList;
 
-/**
- *
- * @author Adam
- */
 public class Block {
     private int numero;
-    private 
-    private int MAX = 1000;
+    private ArrayList suivant;
+    private final int MAX = 1000;
     private int taille;
-    public String enregistrement[];
+    public ArrayList enregistrement = new ArrayList();
     
-    public Block(){
-        
+    public Block(int num){
+        this.numero = num;
+        this.taille = 0;
     }
     
+    public int getNumero(){
+        return this.numero;
+    }
     
+    private Object getEnregistrement(int i){
+        return this.enregistrement.get(i);
+    }
+    
+    private void setEnregistrement(String s){
+        this.enregistrement.add(s);
+    }
+    
+    private Object getSuivant(int i){
+        return this.suivant.get(i);
+    }
+    
+    private void  setSuivant(int i){
+        this.suivant.add(i);
+    }
+    
+    public void addEnregistrement(String s, int i){
+        if(taille + (s.getBytes().length) <= this.taille){
+            this.setEnregistrement(s);
+            this.taille += s.getBytes().length;
+            this.setSuivant(i);
+        }
+        else{
+            System.out.println("Error : Taille de bloc trop petite.");
+        }
+    }
+    
+    public int removeEnregistrement(String s){
+        int i = 0;
+        while(i < this.enregistrement.size()){
+            if(s.contains((String) this.enregistrement.get(i))){
+                this.taille -= s.getBytes().length;
+                this.enregistrement.remove(i);
+                return (int) this.suivant.get(i);
+            }
+        }
+        return -1;
+    }
 }
