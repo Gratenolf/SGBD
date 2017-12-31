5a908f3e505e1b51a020e86b4bc8d34a51028fed
adamTechnoWeb <adm.haddou@gmail.com>
2017-12-31 12:12:32 +0100

    Ajout switch case

--- ProjetSGBD/src/projetsgbd/Serveur.java
+++ ProjetSGBD/src/projetsgbd/Serveur.java
@@ -10,32 +10,46 @@ package projetsgbd;
  * @author Adam
  */
 public class Serveur extends Thread{
-    public SGA sgabuff;
-    public MemDisque memoireD;
-    public boolean finProgramme;
-    public String requete;
-    public String ResRech;
+    private SGA sgabuff;
+    private MemDisque memoireD;
+    private boolean finProgramme;
+    private String requete;
+    private String ResRech;
     private Block ResRechBlock; 
+    private int choixrequete;
     
     public Serveur(SGA sgabuff,MemDisque memoireD){
         this.sgabuff = sgabuff;
         this.memoireD = memoireD;
         finProgramme=false;
         ResRech="Pas";
+        choixrequete=1;
+    }
+    
+    public void update(String reqAvant,String reqApres){
+        
     }
     public void recupBuffer(String requete){
-        System.out.println("recupBuffer requete: "+requete);
+        //System.out.println("recupBuffer requete: "+requete);
         ResRech=sgabuff.rechBlock(requete);
     }
     public void recupDisque(String requete){
-        System.out.println("recupDisque requete: "+requete);
+        //System.out.println("recupDisque requete: "+requete);
         ResRechBlock=memoireD.getElemMemDisque(requete);
     }
     public void run(){
-        while(finProgramme==false){
-                requete="azertyuiopazertyuiopazertyuiop";
-                System.out.println("Serveur s'est réveillé");
+        while(choixrequete<2){//finProgramme==false){
+            switch(choixrequete){
+                case 1: requete="baaaaaaaaabaaaaaaaaabaaaaaaaapbaaaaaaaaabaaaaaaaaabaaaaaaaapbaaaaaaaaabaaaaaaaaabaaaaaaaapbaaaaaaaaabaaaaaaaaabaaaaaaaap";break;
+                case 2: requete="azertyuiopazertyuiopazertyuioa";break;
+                case 3: requete="azertyuiopazertyuiopazertyuiob";break;
+                case 4: requete="azertyuiopazertyuiopazertyuioc";break;
+                case 5: requete="azertyuiopazertyuiopazertyuiod";break;
+            }
+                
+                System.out.println("Serveur s'est réveillé la requete: "+requete);
                 recupBuffer(requete);
+                System.out.println("ResRech: "+ResRech);
                 if(ResRech.equals("Pas"))
                     System.out.println("Erreur pas de recherche");
                 else if(ResRech.equals("Pas de res")){
@@ -54,10 +68,12 @@ public class Serveur extends Thread{
                             //Ecriture dans la mémoire buffercache de SGA
                         }
                 }
-                else
+                else{
                     System.out.println("Un resultat venant de la mémoire centrale!");
-                
-            
+                }
+                choixrequete+=1;
+                ResRech="Pas";
+                sgabuff.update(requete, "aaaaaaaaaaaaaaaaaaaaaaaaaa");
         }
     }
 }
